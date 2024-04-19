7d5cc45abb1d9799451eed3b74bd1b56fed01d03
Stanpac <fritschstanislas@gmail.com>
2024-04-19 09:04:53 +0200

    Rectangle_Mesh Avec 6 Point

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,28 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    auto const triangle_mesh = gl::Mesh{{
+    auto const rectangle_mesh = gl::Mesh{{
         .vertex_buffers = {{
-            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .layout = {gl::VertexAttribute::Position2D{0}},
             .data   = {
-            -1.f, -1.f, // Position2D du 1er sommet
-            +1.f, -1.f, // Position2D du 2ème sommet
-             0.f, +1.f  // Position2D du 3ème sommet
+                -0.5f, -0.5f, // Position2D du 1er sommet
+                +0.5f, -0.5f, // Position2D du 2ème sommet
+                +0.5f, +0.5f, // Position2D du 3ème sommet
+                +0.5f, +0.5f, // Position2D du 4ème sommet
+                -0.5f, -0.5f, // Position2D du 5ème sommet
+                -0.5f, +0.5f  // Position2D du 6ème sommet
             },
         }},
     }};
 
+
     while (gl::window_is_open())
     {
         // Rendu à chaque frame
-        gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
-        triangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
         glClearColor(0.f, 1.f, 0.f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
         glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
 
+        gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
+        rectangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
     }
 }
