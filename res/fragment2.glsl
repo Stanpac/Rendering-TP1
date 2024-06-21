713d0c8d5dd5d4620f0ce5002771b457d9acbd67
Stanpac <fritschstanislas@gmail.com>
2024-06-21 12:54:34 +0200

    Affichage mesh bateau

--- res/fragment2.glsl
+++ res/fragment2.glsl
@@ -9,7 +9,5 @@ in vec2 uv;
 void main()
 {
     vec4 texture_color = texture(my_texture, uv);
-    float gray = dot(texture_color.rgb, vec3(0.3));
-    out_color = vec4 (vec3 (gray), texture_color.a);
-    //out_color =  vec4 (1 - texture_color.rgb, 1.) ;
+    out_color = texture_color;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
 
+#include "tiny_obj_loader.h"
+#include <iostream>
+
 int main()
 {
     // Initialisation
@@ -118,6 +121,80 @@ int main()
     }},
     });
 
+    auto path = gl::make_absolute_path("res/mesh/fourareen.obj");
+    std::string inputfile = path.generic_string();
+
+    tinyobj::ObjReaderConfig reader_config;
+    reader_config.mtl_search_path = "./"; // Path to material files
+
+    tinyobj::ObjReader reader;
+
+    if (!reader.ParseFromFile(inputfile, reader_config)) {
+        if (!reader.Error().empty()) {
+            std::cerr << "TinyObjReader: " << reader.Error();
+        }
+        exit(1);
+    }
+
+    if (!reader.Warning().empty()) {
+        std::cout << "TinyObjReader: " << reader.Warning();
+    }
+
+    auto& attrib = reader.GetAttrib();
+    auto& shapes = reader.GetShapes();
+    auto& materials = reader.GetMaterials();
+
+    std::vector<float> floatArray;
+
+    // Loop over shapes
+    for (size_t s = 0; s < shapes.size(); s++) {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+        for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+            size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+
+            // Loop over vertices in the face.
+            for (size_t v = 0; v < fv; v++) {
+            // access to vertex
+            tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+            floatArray.push_back(attrib.vertices[3*size_t(idx.vertex_index)+0]);
+            floatArray.push_back(attrib.vertices[3*size_t(idx.vertex_index)+1]);
+            floatArray.push_back(attrib.vertices[3*size_t(idx.vertex_index)+2]);
+
+            // Check if `normal_index` is zero or positive. negative = no normal data
+           /*if (idx.normal_index >= 0) {
+                floatArray.push_back(attrib.normals[3*size_t(idx.normal_index)+0]);
+                floatArray.push_back(attrib.normals[3*size_t(idx.normal_index)+1]);
+                floatArray.push_back(attrib.normals[3*size_t(idx.normal_index)+2]);
+            }*/
+
+            // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+            if (idx.texcoord_index >= 0) {
+                floatArray.push_back(attrib.texcoords[2*size_t(idx.texcoord_index)+0]);
+                floatArray.push_back(attrib.texcoords[2*size_t(idx.texcoord_index)+1]);
+            }
+
+            // Optional: vertex colors
+            // tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
+            // tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
+            // tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
+            }
+            index_offset += fv;
+
+            // per-face material
+            shapes[s].mesh.material_ids[f];
+        }
+    } 
+
+    auto const test_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position3D{0}, gl::VertexAttribute::UV{1}},
+            .data   = floatArray,
+        }}
+        
+    }};
+
+    
     while (gl::window_is_open())
     {
          render_target.render([&]() {
@@ -143,7 +220,8 @@ int main()
             shader.set_uniform("my_texture", texture);
 
             //rectangle_mesh.draw(); 
-            cube_mesh.draw();
+            //cube_mesh.draw();
+            test_mesh.draw();
         });
 
         glClear(GL_DEPTH_BUFFER_BIT);
